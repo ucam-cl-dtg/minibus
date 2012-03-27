f0eadd3238db67a036551abd501221521fbf71a3
Andrew Rice <acr31@cam.ac.uk>
2012-03-27 12:22:55 +0100

    Minor change: Added a generated serial version uid to remove the eclipse warning

--- src/uk/ac/cam/cl/dtg/android/time/BusTimetables/DataStoreException.java
+++ src/uk/ac/cam/cl/dtg/android/time/BusTimetables/DataStoreException.java
@@ -2,6 +2,8 @@ package uk.ac.cam.cl.dtg.android.time.BusTimetables;
 
 public class DataStoreException extends Exception {
 
+	private static final long serialVersionUID = -742344923432237299L;
+
 	public DataStoreException(String string) {
 		super(string);
 	}
