cc0748a544a85655928a117b1f9884100a1d4178
Andrew Rice <acr31@cam.ac.uk>
2012-06-05 19:36:24 +0100

    Major change: modified the build system to use the new android parent pom.  This separates configuration into src-debug and src-release.  Updated the MapViewActivity to create the map programmatically so we can do substitution.

--- src/uk/ac/cam/cl/dtg/android/time/Constants.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.cam.cl.dtg.android.time;
-
-public class Constants {
-
-	public static final String OMNIBUS_FEEDURL = "${omnibus.feedurl}";
-	public static final String OMNIBUS_APIKEY = "${omnibus.apikey}";
-	public static final boolean DEBUG = Boolean.parseBoolean("${debug}");
-}
