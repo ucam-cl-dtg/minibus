5c3d2a01a64c5396d4b6b0108a22450137aab302
Daniel Thomas <drt24@cam.ac.uk>
2012-06-14 09:16:52 +0100

    Fix release constants file so that it compiles...
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- src-release/uk/ac/cam/cl/dtg/android/time/Constants.java
+++ src-release/uk/ac/cam/cl/dtg/android/time/Constants.java
@@ -4,9 +4,9 @@ import uk.ac.cam.cl.dtg.compilesettingsloader.Loader;
 
 public class Constants {
 
-	public static final String OMNIBUS_FEEDURL = ${omnibus.feedurl}
-	public static final String OMNIBUS_APIKEY = ${omnibus.apikey}
-	public static final String MAPS_APIKEY = ${omnibus.releasemapskey}
+	public static final String OMNIBUS_FEEDURL = "${omnibus.feedurl}";
+	public static final String OMNIBUS_APIKEY = "${omnibus.apikey}";
+	public static final String MAPS_APIKEY = "${omnibus.release.mapskey}";
 	public static final boolean DEBUG = false;
 	
 }
